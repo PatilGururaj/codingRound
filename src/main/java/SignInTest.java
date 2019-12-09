c3449dc9356292d32b52b66259a254d6151f1079
Gururaj Patil <guru21patil@gmail.com>
2019-12-09 17:45:20 +0530

    Fixed the issues from the SignInTest class Replaced the chromeDriver with the version 78 Uploaded the Comments file

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,25 +1,34 @@
 import com.sun.javafx.PlatformUtil;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignInTest {
 
-    WebDriver driver = new ChromeDriver();
-
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
-        setDriverPath();
+        WebDriver driver = setDriverPath();
+        driver.manage().window().maximize();
 
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        int size = driver.findElements(By.tagName("iframe")).size();
+        System.out.println(size);
+        List <WebElement> iframes = driver.findElements(By.tagName("iframe"));
+        WebElement frameElement = driver.findElement(By.xpath("//div[@id='ModalFrame']//iframe"));
+        String frameName = frameElement.getAttribute("name");
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame(frameName);
         driver.findElement(By.id("signInButton")).click();
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
@@ -35,16 +44,20 @@ public class SignInTest {
         }
     }
 
-    private void setDriverPath() {
+    private WebDriver setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
+            return new ChromeDriver();   
         }
         if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            return new ChromeDriver();            
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+            return new ChromeDriver();   
         }
+        return null;
     }
 
 
