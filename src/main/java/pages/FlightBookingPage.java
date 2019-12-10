f174225be20db93b69a06df6f227a9d412a5262b
Gururaj Patil <guru21patil@gmail.com>
2019-12-10 13:26:18 +0530

    Refactor'd the FlightBookingTest class and added a new class
    FlightBookingPage

---
+++ src/main/java/pages/FlightBookingPage.java
@@ -0,0 +1,65 @@
+/**
+ * g.patil
+ */
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * @author g.patil
+ *
+ */
+public class FlightBookingPage {
+	
+	private WebDriver driver;
+	
+	@FindBy(id="OneWay")
+	WebElement radioButtonOneWay;
+	
+	@FindBy(id="FromTag")
+	WebElement txtBoxFrom;
+	
+	@FindBy(id="ToTag")
+	WebElement txtBoxTo;
+	
+	@FindBy(xpath = "//ul[@id='ui-id-1']")
+	WebElement dDowndestOptions;
+	
+	@FindBy(xpath="//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")
+	WebElement datePicker;
+	
+	@FindBy(id="SearchBtn")
+	WebElement btnSearchFlights;
+	
+	public FlightBookingPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void searchForOneWayFlights(){
+		radioButtonOneWay.click();
+		txtBoxFrom.clear();
+		txtBoxFrom.sendKeys("Bangalore");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+		originOptions.get(0).click();
+		txtBoxTo.clear();
+		txtBoxTo.sendKeys("Delhi");
+		List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
+		destinationOptions.get(0).click();
+		datePicker.click();
+		btnSearchFlights.click();
+	}
+	
+
+}
--- src/main/java/tests/FlightBookingTest.java
+++ src/main/java/tests/FlightBookingTest.java
@@ -1,32 +1,33 @@
 package tests;
-import com.sun.javafx.PlatformUtil;
+
+import pages.FlightBookingPage;
+import utiity.Base;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-public class FlightBookingTest {
+public class FlightBookingTest extends Base{
 
     
 	
-	WebDriver driver;
+	/*WebDriver driver;
 	@BeforeTest
 	public void inititalizeDriver(){
 		
-	}
+	}*/
 
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
+    	
+    	FlightBookingPage flightBooking = new FlightBookingPage(driver);
+    	navigateToApplication("https://www.cleartrip.com/");
+    	waitFor(2000);
+    	flightBooking.searchForOneWayFlights();
 
-        setDriverPath();
+        /*driver = setDriverPath();
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
@@ -36,13 +37,12 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        waitFor(5000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
-
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
         //wait for the auto complete options to appear for the destination
 
         waitFor(2000);
@@ -53,7 +53,7 @@ public class FlightBookingTest {
         driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
 
         //all fields filled in. Now click on search
-        driver.findElement(By.id("SearchBtn")).click();
+        driver.findElement(By.id("SearchBtn")).click();*/
 
         waitFor(5000);
         //verify that result appears for the provided journey search
@@ -83,15 +83,17 @@ public class FlightBookingTest {
         }
     }
 
-    private void setDriverPath() {
+    /*private WebDriver setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
         if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            return new ChromeDriver();
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
-    }
+        return null;
+    }*/
 }
